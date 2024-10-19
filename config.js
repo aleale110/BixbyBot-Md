8316353b79f9c28201c05e99e80d661518b94033
aleale110 <alejandrorusso170910@gmail.com>
2024-10-20 00:41:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
+  ['+27719626577', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
   ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
